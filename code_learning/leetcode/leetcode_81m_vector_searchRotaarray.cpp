d617c15d186d8aa812e11ac277ddc4d7ae05be65
Tcloser <xunjtech@gmail.com>
2021-02-18 14:12:40 +0800

    update leetcode81

--- code_learning/leetcode/leetcode_81m_vector_searchRotaarray.cpp
+++ code_learning/leetcode/leetcode_81m_vector_searchRotaarray.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 /* -------------------------------------------
  * 拆分查找 折半查找
+ * 关键点：
+ * 特殊类型判定【重复数字太多的】
+ * 不确定单调性时的左右区间判断，说明折半查找必须要有`明显的单调性存在` 否则毫无办法 先sort
  * ------------------------------------------*/
 bool searchRota(vector<int> nums,int target)
 {
@@ -15,24 +18,24 @@ bool searchRota(vector<int> nums,int target)
     {
         int mid = (high+low)/2;
         if(nums[mid] == target) return true;
-        // 取右侧
-        if(nums[mid]<=nums[high])
+        if(nums[min] == nums[low]) low++;// [1,1,1,1,1,2,1,1] 这个最难想
+        // 若中间值要比最右侧小 说明右侧单调 若相等则说明存在重复
+        // 取右侧部分
+        else if(nums[mid]<=nums[high])
         {
+            // 通过两端值判断确定目标值是否在右侧
             if(nums[mid] < target && nums[high]>=target){
+                // 取右侧折半
                 low = mid +1;
             }
-            else{
-                high = mid - 1;
-            }
+            else high = mid - 1;
         }
         else
         {
             if(nums[mid] > target && nums[low]<=target){
                 high = mid - 1;
             }
-            else{
-                low = mid +1;
-            }
+            else low = mid +1;
         }
     }
     return false;
