3579dbfc2a76011da7b6ab944dbb2e77fd5aacaa
Tcloser <xunjtech@gmail.com>
2021-02-19 14:38:25 +0800

    add leetcode198

---
+++ code_learning/leetcode/leetcode_198m_DP_houserobber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* -------------------------------------------
+ * 注意：
+ * temp当前位置存储的是可抢劫的最大值 不能是house0 house1的值
+ * ------------------------------------------*/
+
+int rob(vector<int>& house)
+{
+    if(house.empty())return 0;
+    if(house.size() == 1)return house[0];
+    if(house.size() == 2)return max(house[0],house[1]);
+
+    vector<int>temp{house[0],max(house[0],house[1])};
+    for(int i = 2;i<house.size();i++)
+    {
+        int maxsum = max(temp[i-1],temp[i-2]+house[i]);
+        temp.push_back(maxsum);
+    }
+
+    return temp[-1];
+}
+
+int main()
+{
+    vector<int> house{2,1,1,2};
+
+    int result = rob(house);
+
+    return 0;
+}
