e8cc8f4b7e83a803f7af131c6ea3dbe0de0c83ce
Tcloser <xunjtech@gmail.com>
2021-02-25 14:02:03 +0800

    update leetcode62

--- code_learning/leetcode/leetcode_62m_DP_uniquePath.cpp
+++ code_learning/leetcode/leetcode_62m_DP_uniquePath.cpp
@@ -4,26 +4,26 @@
 using namespace std;
 
 /* -------------------------------------------
- * 注意：ac 100% 47.75%
+ * 注意：ac 100% 53.75%
  * 第n个位置可由左边或者上面走过来的
  * ------------------------------------------*/
 
 int uniquePath(int m,int n)
 {
     vector<vector<int>>grid(m,vector<int>(n,0));
-    grid[0][0] =  1;
 
     for(int i = 0;i<m;i++)
     {
         for(int j = 0;j<n;j++)
         {
-            if(i ==0&&j==0) continue;
-            if((i-1)>=0 && j-1>=0)
-                grid[i][j] = grid[i][j-1] + grid[i-1][j];
-            else if((i-1)<0 && (j-1)>=0)
+            if(i ==0&&j==0)
+                grid[i][j] =  1;
+            else if(i==0 && j>0)
                 grid[i][j] = grid[i][j-1];
-            else
+            else if(i>0 && j==0)
                 grid[i][j] = grid[i-1][j];
+            else
+                grid[i][j] = grid[i][j-1] + grid[i-1][j];
         }
     }
     return grid[m-1][n-1];
