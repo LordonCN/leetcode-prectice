e243db14b0e90ad0c76aed54b5bed1bc2e1e178a
Tcloser <xunjtech@gmail.com>
2021-02-26 11:06:23 +0800

    update leetcode3 ac

--- code_learning/leetcode/leetcode_03m_string_lengthOfLongestSubstring.cpp
+++ code_learning/leetcode/leetcode_03m_string_lengthOfLongestSubstring.cpp
@@ -3,60 +3,77 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 /* -------------------------------------------
- * 用map更适合我
- * 亮点：
- * 测试set效果
+ * 1-hashmap O(n2)
+ * 2-deque+hashmap O(n) // 初步测试失败了 hashmap并不是按顺序添加进去的 但是可以单个擦除 我笑了
+ * 注意：ac 44.4%  13.0%
  * ------------------------------------------*/
-
-void lengthOfLongestSubstring(string s)
+int lengthOfLongestSubstring(string s)
 {
-    vector<char>stringStore;
-    int maxLength = 0;
     unordered_map<char,int> hashmap;
-
+    deque<int>dq;
+    int maxLength = 0;
     // 从第一个字符遍历
     for(int i = 0;i<s.length();i++)
     {
-        hashmap.erase(hashmap.begin(),hashmap.end());
-        hashmap[s[i]] ++;
-        for(int k = i+1;k<s.length();k++)
+        // 未找到该重复字符 添加
+        if(hashmap.find(s[i])==hashmap.end())
         {
-            hashmap[s[k]] ++;
-            if(2 == hashmap[s[k]])
-            {
-                maxLength = maxLength>hashmap.size()? maxLength : hashmap.size();
-                break;
-            }
+            dq.push_back(s[i]);
+            hashmap[s[i]] ++;
+            maxLength = maxLength>hashmap.size()? maxLength : hashmap.size();
         }
-
+        else
+        {
+            // 更新dq 以及 hashmap中元素 这里时间耗费挺多 但是不到O(n)
+            // 当前重复字符为s[i] hashmap中位置为hashmap.find(s[i])
+            while(dq.front()!=s[i])
+            {
+                hashmap.erase(hashmap.find(dq.front())); //单个元素擦除
+                dq.pop_front();
+            }
+            hashmap.erase(hashmap.find(dq.front()));
+            dq.pop_front();
+            // 把最前面的退出来放到后面
+            dq.push_back(s[i]);
+            hashmap[s[i]] ++;
+        }
     }
-    return ;
+    return maxLength;
 }
-
-void lengthOfLongestSubstring2(string s)
+/* -------------------------------------------
+ * 测试hashset功能 非本题题解
+ * ------------------------------------------*/
+int lengthOfLongestSubstring2(string s)
 {
-    vector<char>stringStore;
     int maxLength = 0;
     set<char>hashset;
 
-    // 只测试插入操作
+    // 只测试插入操作 使用指针
     for(auto it = s.begin();it<s.end();it++)
     {
-        hashset.erase(hashset.begin(),hashset.end());
-        hashset.insert(*it);
-        hashset.find(*it);
+        if(hashset.find(*it) != hashset.end())
+        {
+            hashset.insert(*it);
+            maxLength = maxLength>hashset.size()?maxLength:hashset.size();
+        }
+        else
+            hashset.erase(hashset.begin(),hashset.end());
+
+
     }
-    return ;
+    return maxLength;
 }
+
 int main()
 {
-    string s = "abcdefababab";
+    string s = "kubdypzxdivrqa";
 
-    lengthOfLongestSubstring2(s);
+    lengthOfLongestSubstring(s);
 
 
     return 0;
