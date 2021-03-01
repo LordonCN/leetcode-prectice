87d3d2dcae36b474b8a90272e758068ec9f1b828
Tcloser <xunjtech@gmail.com>
2021-03-01 22:43:06 +0800

    update leetcode451 二刷

--- code_learning/leetcode/leetcode_451m_hashsort_string_frequencySort.cpp
+++ code_learning/leetcode/leetcode_451m_hashsort_string_frequencySort.cpp
@@ -6,9 +6,10 @@
 using namespace std;
 /* -------------------------------------------
  * cpp哈希表的创建方法unordered_map<int,int> hashMap;
- * hash  遍历方法for (auto n : hashMap)
+ * hash  遍历方法for (auto [c,v] : hashMap) vector.push_back({c,v})
  * vector 一行创建vector<int> nums {4,4,0,0,4,1,6,9,8,13};
  * vector 遍历方法for(auto number : nums)
+ * 以及二刷出现的问题
  * ------------------------------------------*/
 bool sortVector2d(const vector<int>a,const vector<int>b)
 {
@@ -45,6 +46,38 @@ void frequencySort(string S)
     // return output 即可
 
     return ;
+}
+
+string methord451(string s)
+{
+    if(s.size() == 0 )return "";
+    unordered_map<char,int>hashmap;
+
+    // 首先统计出现频率
+    for(auto i : s)
+        hashmap[i] += 1;
+
+    // 这里出现了二维数组赋值的常见错误 把第一维度的vector当成了变量
+    /*************************************************************************
+    vector<vector<int>>store(hashmap.size(),vector<int>(1,0));
+    for(auto i : s)
+        store[i-'a'][0] = hashmap[i];
+
+    sort(store.begin(),store.end(),[](vector<int>a,vector<int>b){return a[0]>b[0];});
+    *======================================================================*/
+    vector<vector<int>>store;
+    for(auto [c,v] : hashmap)
+        store.push_back({c,v});
+
+    // 对二维数组第二个位置进行排序
+    sort(store.begin(),store.end(),[](vector<int>a,vector<int>b){return a[1]>b[1];});
+
+    string result;
+    for(auto i : store)
+        while(i[1]--)
+            result += i[0] - 'a' + 'a';// 恢复成字符
+
+    return result;
 }
 
 int main()
