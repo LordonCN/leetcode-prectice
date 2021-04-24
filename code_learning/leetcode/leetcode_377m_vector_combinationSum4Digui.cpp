5bba1c8b0e6e9b9f6f9068e5bf99810c8bbdc07d
Macmini <xunjtech@gmail.com>
2021-04-24 21:03:25 +0800

    add combin dp

--- code_learning/leetcode/leetcode_377m_vector_combinationSum4Digui.cpp
+++ code_learning/leetcode/leetcode_377m_vector_combinationSum4Digui.cpp
@@ -39,3 +39,18 @@ int combinationSum4(vector<int>& aim, int target) {
     }
     return result.size();
 }
+
+// 需要考虑顺序问题 将数组放里面遍历
+// ac 组合+排序 动态规划过程中需要
+// TODO:这个最大值阈值设置有点奇怪？？？
+int combinationSum4(vector<int>& nums, int target) {
+    vector<int>dp(target+1,0);
+    dp[0] = 1;
+    for(int i = 1;i<=target;i++)
+    {
+        for(int j = 0;j<nums.size();j++)
+            if(i>=nums[j] && dp[i] < INT_MAX - dp[i - nums[j]])
+                dp[i] = dp[i] + dp[i-nums[j]];
+    }
+    return dp[target];
+}
