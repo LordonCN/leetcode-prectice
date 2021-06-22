5e369fc4b58d8886c42887d49675ab18e437d902
Macmini <xunjtech@gmail.com>
2021-06-22 10:43:09 +0800

    add kalman filter

--- code_learning/basic/kalman-fliter.cpp
+++ code_learning/basic/kalman-fliter.cpp
@@ -1,63 +1,40 @@
 // gaussian noise generate
-#include <math.h>
-#include <numeric>
 #include <iostream>
+#include <math.h>
+#include <random>// normal_distribution
+#include <time.h>
 
 /* -------------------------------------------
  * 生成高斯噪声
  * 网上找的 其实是固定数的噪声
- * ------------------------------------------*/
-double generateGaussianNoise(double mu, double sigma)
-{
-    const double epsilon = std::numeric_limits<double>::min();
-    const double two_pi = 2.0*3.14159265358979323846;
-
-    static double z0, z1;
-    static bool generate;
-    generate = !generate;
-
-    if (!generate)
-        return z1 * sigma + mu;
-
-    double u1, u2;
-    do
-    {
-        u1 = rand() * (1.0 / RAND_MAX);
-        u2 = rand() * (1.0 / RAND_MAX);
-    } while (u1 <= epsilon);
-
-    z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
-    z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
-    return z0 * sigma + mu;
-}
-
-/* -------------------------------------------
- * kalman filter
+ * Q 影响滤波后曲线形状 往小调平滑性增高
+ * R 对观测值的信任程度
+ * resrcdata[0] 不敏感 给的区别很大也没有关系
  * ------------------------------------------*/
 float Kalman_Filter(const float ResrcData,float ProcessNiose_Q,float MeasureNoise_R)
 {
     float R = MeasureNoise_R;
     float Q = ProcessNiose_Q;
+    float k = 0.01;
+    float h = 0.95;// 能够明显改变滤波倾向大小
+    float F1 = 1.0;
 
     static float x_last = 0.01;// 存储之前的值
     static float p_last = 0.01;
 
-    float x_mid;
+    float x_min;
     float x_now;
 
-    float p_mid;
+    float p_min;
     float p_now;
 
-    float k = 0.01;
-    float h = 0.95;// 能够明显改变滤波倾向大小
+    x_min = x_last * F1; //
+    p_min = p_last * F1 * F1 + Q; //Q=噪声
 
-    x_mid = x_last; //
-    p_mid = p_last + Q; //Q=噪声
+    k = h * p_min / (h * h * p_min+R); //kg为kalman filter，R为噪声
 
-    k = h * p_mid / (h * h * p_mid+R); //kg为kalman filter，R为噪声
-
-    x_now = x_mid + k * (ResrcData - h * x_mid);//估计出的最优值
-    p_now = (1 - k * h) * p_mid;//最优值对应的covariance
+    x_now = x_min + k * (ResrcData - h * x_min);//估计出的最优值
+    p_now = (1 - k * h) * p_min;//最优值对应的covariance
 
     p_last = p_now; //更新covariance值
     x_last = x_now; //更新系统状态值
@@ -65,16 +42,26 @@ float Kalman_Filter(const float ResrcData,float ProcessNiose_Q,float MeasureNois
     return x_now;
 }
 
+void kalman_test()
+{
+    int n = 100;
+    vector<double>state(n,0);
+
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::normal_distribution<double> distribution(0.0, 0.2);// 期望0 方差0.2
+
+    for(int i = 0;i<n;i++)
+        state[i] = i + distribution(generator);
+
+    for(int i = 0;i<n;i++)
+        state[i] = Kalman_Filter(state[i], 0.2, 0.1);// QR需要不断自己调 经验值
+    int a = 0;
+}
+
 int main()
 {
-    vector<double>state(10,0);
-
-    for(int i = 0;i<10;i++)
-        state[i] = 3 * i + generateGaussianNoise(0, 0.5);// mu sigma
-
-        // 有效果
-    for(int i = 0;i<10;i++)
-        state[i] = Kalman_Filter(state[i], 0.22, 0.1);
+    kalman_test();
 
     return 0;
 }
